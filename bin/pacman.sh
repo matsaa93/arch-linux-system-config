e3b901c710a115c9052a025f184bf593ae0912d2
matsaa93 <matsaa93@live.no>
2017-10-23 19:59:16 +0200

    added more fixes and missing options done some cleanup TODO: clean up some more fix boot.sh make a menu for the functions

--- bin/pacman.sh
+++ bin/pacman.sh
@@ -11,7 +11,7 @@ ins_repo() { ### REPOSETORY INSTALLL
     cd $pacc
     for r in $repos
     do
-        cat $r >> /etc/pacman.conf
+        cat $r >> /etc/pacman.conf && utlog $r
     done
     popd
     Pm_db
@@ -30,31 +30,31 @@ ins_libreoffice() { ### Libre Office Liberate your Office ;P
 #
 ins_alsa() { ### ALSA DRIVER & LIBs
     echo "Alsa"
-    ins="alsa-lib alsa-oss alsa-plugins alsa-tools alsa-utils jack2 jack2-dbus" && Pm_i
+    Pm_i "alsa-lib alsa-oss alsa-plugins alsa-tools alsa-utils jack2 jack2-dbus"
     echo "Alsa 32-bit-lib"
-    ins="lib32-alsa-oss lib32-alsa-plugins lib32-alsa-lib" && Pm_i
+    Pm_i "lib32-alsa-oss lib32-alsa-plugins lib32-alsa-lib"
 }
 #
 ins_codecs() { ### MULTIMEDIA CODEC's
     echo "multimedia codec"
-    ins="opus celt faac faad2 flac jasper lame a52dec libdca libdv libmpeg2 libmad libmpcdec opencore-amr schroedinger speex libtheora libvorbis libvpx wavpack x264 x265 xvidcore libfdk-aac" && Pm_i
-    ins="exfat-utils fuse-exfat gst-libav libxv gstreamer0.10-plugins flashplugin libdvdcss libdvdread libdvdnav dvd+rw-tools dvdauthor dvgrab" && Pm_i
+    Pm_i "opus celt faac faad2 flac jasper lame a52dec libdca libdv libmpeg2 libmad libmpcdec opencore-amr schroedinger speex libtheora libvorbis libvpx wavpack x264 x265 xvidcore libfdk-aac"
+    Pm_i "exfat-utils fuse-exfat gst-libav libxv gstreamer0.10-plugins flashplugin libdvdcss libdvdread libdvdnav dvd+rw-tools dvdauthor dvgrab"
     ### XINE AND FFMPEG
     echo "xine ffmpeg"
-    ins="xine xine-lib ffmpeg" && Pm_i
+    Pm_i "xine xine-lib ffmpeg"
 }
 #### YAOURT (AUR)
-ins_yaourt() { echo "yaourt" && ins="yaourt" && Pm_i }
+ins_yaourt() { echo "yaourt" && Pm_i "yaourt" }
 #### VLC VideoLAN Media Player
-ins_vlc() { echo "vlc" && ins="vlc" && Pm_i }
+ins_vlc() { echo "vlc" && Pm_i "vlc" }
 #### Pacman Gui Installer
-ins_pacgui() { echo "pacman Gui" && ins="pamac-aur" && Pm_i }
+ins_pacgui() { echo "pacman Gui" && Pm_i "pamac-aur" }
 #### ARCHIVING TOOLS
-ins_archive() { echo "archiving" && ins="p7zip p7zip-plugins unrar tar rsync" && Pm_i }
+ins_archive() { echo "archiving" && Pm_i "p7zip p7zip-plugins unrar tar rsync" }
 #### Acess Control List
-ins_acl() { echo "Access Control List" && ins="acl" &&  Pm_i }
+ins_acl() { echo "Installing Access Control List {acl}" &&  Pm_i "acl"  }
 #### Bluetooth
-ins_bluez() { echo "bluez" && ins="bluez bluez-utils" && Pm_i }
+ins_bluez() { echo "Installing Bluez & it's utils" && Pm_i "bluez bluez-utils" }
 #
 ins_mics() { echo "mics" && Pm_i "lib32-libldap" }
 ##
@@ -81,41 +81,61 @@ auto_pacman() {
     ins_readedid
     echo "Finished AUTO FUNCTION pacman.sh"
 }
-
-ins_OhMyZsh() {
-    echo "START $0"
-echo "INSTALLING ZSH"
-    Pm_i "zsh zsh-completions"
-    chsh -s /usr/bin/zsh
-    cp -aT /etc/skel/ /root/
+ins_Zsh() {
+    echo "INSTALLING ZSH" && Pm_i "zsh zsh-completions"
+    chsh -s /bin/zsh
+    cp -aT /etc/skel /root/
     chmod 700 /root
-    Pm_i "git wget"
-echo "INSTALLING OH_MY_ZSH"
-    #powerline powerline-fonts oh-my-zsh
-    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-echo "INSTALLING DEP POWERLINE "
-    Pm_i "powerline powerline-fonts powerline-vim python-powerline"
-    Pm_i "awesome-terminal-fonts"
-    Pm_i "fontforge"
-     git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-     git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-echo "POWERLINE INSTALL DEP FINALIZED"
-    # cp -r {.zshrc,.fonts,.oh-my-zsh} ~/
-    cp -r $arzsh/power-themes ~/.oh-my-zsh/custom/themes/
-    cp -r $arzsh/fontconfig ~/.config/
-    # fc-cache -fv ~/.fonts
-    cat $arzsh/.zshrc > ~/.zshrc
-echo "OH-MY-ZSH INSTALL FINALIZED"
-echo "INSTALLING VIM"
-    Pm_i "vim vim-plugins"
+}
+ins_git() { echo "INSTALLING git & wget" && Pm_i "git wget" }
+ins_powerline() { echo "INSTALLING POWERLINE" && Pm_i "powerline powerline-fonts powerline-vim python-powerline" }
+ins_fontforge() { echo "INSTALLING awesome-terminal-fonts" && Pm_i "awesome-terminal-fonts fontforge" }
+ins_vim() {
+    echo "INSTALLING vim" && Pm_i "vim vim-plugins"
     git clone https://github.com/amix/vimrc.git ~/.vim_runtime && sh ~/.vim_runtime/install_awesome_vimrc.sh
-echo "VIM INSTALL FINALIZED"
-echo "COPYING .zshrc,.oh-my-zsh,.vimrc,.vim_runtime to /etc/skel"
-    cp -r ~/{.zshrc,.oh-my-zsh,.vimrc,.vim_runtime} /etc/skel/
-    mkdir -p /etc/skel/.config
-    cp -r $arzsh/fontconfig /etc/skel/.config/
-echo "FINISHED copying all files"
-echo "FINIDHED $0"
+}
+isinstalled() {
+    if [ -f /bin/$1 ]; then
+        echo true
+            elif [ -f /usr/bin/$1 ]; then
+                echo true
+                else
+                    echo false
+fi
+}
+ins_OhMyZsh() {
+    echo "INSTALLING/Checking Dependencies Availability"
+    echo "checking if is installed ZSH"
+        [ $(isinstalled zsh) = true ] || ins_Zsh
+        echo "checking if is installed git"
+        [ $(isinstalled git) = true ] || ins_git
+        echo "checking if is installed powerline"
+        [ $(isinstalled powerline) = true ] || ins_powerline
+        echo "checking if is installed fontforge"
+        [ $(isinstalled fontforge) = true ] || ins_fontforge
+        echo "checking if is installed vim"
+        [ $(isinstalled vim) = true ] || ins_vim
+
+
+    echo "INSTALLING OH_MY_ZSH"
+        #powerline powerline-fonts oh-my-zsh
+        sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+    echo "INSTALLING Utils & helpers & extentioms from github"
+        git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+        git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+    echo "POWERLINE INSTALL DEP FINALIZED"
+        # cp -r {.zshrc,.fonts,.oh-my-zsh} ~/
+        cp -r $arzsh/power-themes ~/.oh-my-zsh/custom/themes/
+        cp -r $arzsh/fontconfig ~/.config/
+        # fc-cache -fv ~/.fonts
+        cat $arzsh/.zshrc > ~/.zshrc
+    echo "OH-MY-ZSH INSTALL FINALIZED"
+    echo "COPYING .zshrc,.oh-my-zsh,.vimrc,.vim_runtime to /etc/skel"
+        cp -r ~/{.zshrc,.oh-my-zsh,.vimrc,.vim_runtime} /etc/skel/
+        mkdir -p /etc/skel/.config
+        cp -r $arzsh/fontconfig /etc/skel/.config/
+    echo "FINISHED copying all files"
+    echo "FINIDHED $0"
 }
 
 pacman_menu() {
